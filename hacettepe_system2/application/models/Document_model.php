725280e6cab15885d0beb722ae827e39b8ca28bd
nmutlu98 <53997005+nmutlu98@users.noreply.github.com>
2019-08-30 02:06:20 +0300

    Inserting a newly uploaded doc

--- hacettepe_system2/application/models/Document_model.php
+++ hacettepe_system2/application/models/Document_model.php
@@ -3,6 +3,12 @@ class Document_model extends CI_Model{
 	public function get_document_by_id($id){
 		return $this->db->get_where('documents',array('id'=>$id));
 	}
+	public function add_document($data){
+		return $this->db->insert('documents',$data);
+	}
+	public function get_document_id($doc_name){
+		return $this->db->get_where('documents',array('document_name'=>$doc_name))->row_array()['id'];
+	}
 }
 
 ?>
