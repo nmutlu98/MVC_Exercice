ab1333d002d68e34f201882b65663deca50e0652
nmutlu98 <53997005+nmutlu98@users.noreply.github.com>
2019-09-04 21:16:47 +0300

    checks if a username is used before by another user
    for registration

--- hacettepe_system2/application/models/Login_model.php
+++ hacettepe_system2/application/models/Login_model.php
@@ -5,6 +5,17 @@ class Login_model extends CI_Model{
 		return $this->db->get_where('users',array('user_name'=>$data['username'],'password'=>$data['password']));
 		
 	}
+	public function validate_username($username){
+
+		$usernames = $this->db->get('users')->result_array();
+		$names = array();
+		foreach ($usernames as $usernam) {
+			if($username==$usernam['user_name']){
+				return 0;
+			} 
+		}
+		return 1;
+	}
 }
 
 ?>
