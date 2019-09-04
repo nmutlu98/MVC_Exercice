d3f2f38e2b9d345945a2ea19675a2ed8934214bd
nmutlu98 <53997005+nmutlu98@users.noreply.github.com>
2019-09-04 21:04:50 +0300

    registering new users

--- hacettepe_system2/application/controllers/Login.php
+++ hacettepe_system2/application/controllers/Login.php
@@ -4,6 +4,7 @@ class Login extends CI_Controller{
 	function __construct() {
         parent::__construct();
         $this->load->model("login_model");
+        $this->load->model("user_model");
         $this->load->model("commitee_model");
     }
 
@@ -20,6 +21,35 @@ class Login extends CI_Controller{
 			$this->load->view("main_screen/main_screen",$page_data);
 		}else{
 			echo "Your access is forbidden";
+		}
+	}
+	public function log_out(){
+		$this->session->sess_destroy();
+		$this->load->view('login/login');
+	}
+	public function register(){
+		$this->load->view('login/register');
+	}
+	public function register_user(){
+		$username = htmlspecialchars($this->input->post('username'));
+		$password = htmlspecialchars($this->input->post('password'));
+		$first_name = htmlspecialchars($this->input->post('first_name'));
+		$last_name = htmlspecialchars($this->input->post('last_name'));
+		$email = htmlspecialchars($this->input->post('email'));
+		$page_data['message'] = "";
+		if($this->login_model->validate_username($username)){
+			$page_data['message'] = "Kaydınız tamamlandı.";
+			$data['user_name'] = $username;
+			$data['password'] = $password;
+			$data['first_name'] = $first_name;
+			$data['last_name'] = $last_name;
+			$data['email'] = $email;
+			$res = $this->user_model->register_user($data);
+			if($res)
+				$this->load->view('login/login',$page_data);
+		} else{
+			$page_data['message'] = "Bu kullanıcı adı zaten kullanılmaktadır.";
+			$this->load->view('login/register',$page_data);
 		}
 	}
 	
